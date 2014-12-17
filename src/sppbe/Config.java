6c31a7079809b05d10666d6050ab530212fe7f7e
Ivo Idham Perdameian <ivo.idham@gmail.com>
2014-12-18 03:00:38 +0700

    Config class

---
+++ src/sppbe/Config.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sppbe;
+
+/**
+ *
+ * @author idham
+ */
+public final class Config {
+
+    public static final String SYSTEM_EXIT_TITLE = "Keluar Sistem";
+    public static final String SYSTEM_EXIT_MESSAGE = "Apakah Anda yakin ingin keluar dari sistem?\n\nBila Anda keluar dari aplikasi maka SMS peringatan mungkin tidak bisa disampaikan tepat waktu.\n\nKlik Ya untuk melanjutkan.";
+    public static final String SYSTEM_EXIT_BUTTON_OK = "Ya";
+    public static final String SYSTEM_EXIT_BUTTON_NO = "Tidak";
+}
