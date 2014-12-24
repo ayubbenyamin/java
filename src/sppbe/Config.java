f5fd0328ef897e78c6198dc9954f20eee3aa2c8f
Ivo Idham Perdameian <ivo.idham@gmail.com>
2014-12-25 03:59:09 +0700

    add new config

--- src/sppbe/Config.java
+++ src/sppbe/Config.java
@@ -14,13 +14,15 @@ import javax.persistence.Persistence;
  */
 public final class Config {
 
+    public static final String BUTTON_OK = "Ya";
+    public static final String BUTTON_NO = "Tidak";
     public static final String SYSTEM_EXIT_TITLE = "Keluar Sistem";
-    public static final String SYSTEM_EXIT_MESSAGE = "Apakah Anda yakin ingin keluar dari sistem?\n\nBila Anda keluar dari aplikasi maka SMS peringatan jatuh\ntempo akan sangat mungkin tidak bisa disampaikan tepat waktu.\n\nKlik Ya untuk melanjutkan.";
-    public static final String SYSTEM_EXIT_BUTTON_OK = "Ya";
-    public static final String SYSTEM_EXIT_BUTTON_NO = "Tidak";
+    public static final String SYSTEM_EXIT_MESSAGE = "Apakah Anda yakin ingin keluar dari sistem?\n\nBila Anda keluar dari aplikasi maka SMS peringatan jatuh\ntempo akan sangat mungkin tidak bisa disampaikan tepat waktu.\n\nKlik " + BUTTON_OK + " untuk melanjutkan.";
     public static final String TRAY_MENU_SHOW = "Tampilkan Aplikasi";
     public static final String TRAY_MENU_ABOUT = "Tentang";
     public static final String TRAY_MENU_EXIT = "Keluar Aplikasi";
     public static final String TRAY_TOOLTIP = "Aplikasi SMS Peringatan SPPBE";
     public static final EntityManagerFactory EMF = Persistence.createEntityManagerFactory("SPPBEPU");
+    public static final String REMOVE_RECORD_TITLE = "Hapus data";
+    public static final String REMOVE_RECORD_MESSAGE = "Anda yakin untuk menghapus data yang dipilih?\nKlik " + BUTTON_OK + " untuk melanjutkan";
 }
